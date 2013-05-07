d1ef785de64052223c0fb503595d8dc3d0cc9873
Luis Fernandez Alvarez <luis.fernandez.alvarez@cern.ch>
2013-05-07 13:48:10 +0200

    Changed argument on extract_archive.
    This patch fixes the error 'Invalid parameter archivefile at /etc..'

--- manifests/commands.pp
+++ manifests/commands.pp
@@ -95,9 +95,9 @@ class windows::commands{
     }
   }
 
-  define extract_archive ($::archivefile){
+  define extract_archive ($archivefile){
     exec {"7z_extract_${name}":
-      command => "7z.exe x ${::temp}\\${::archivefile}",
+      command => "7z.exe x ${::temp}\\${archivefile}",
       path    => "${programw6432}\\7-Zip;${::path}",
       cwd     => $::temp,
       require => Package['7z930-x64'],
